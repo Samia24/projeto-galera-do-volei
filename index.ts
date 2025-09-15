9dacc8a4478e2c6ad295c9ba1b599e4594c742be
Samia <samiadantas@hotmail.com>
2025-09-14 23:08:22 -0300

    Adicionadas as rotas de partidas (games) e jogadores (players)

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import express, { Request, Response } from "express";
+import playersRouter from "./routes/players_routes";
+import solicitationsRouter from "./routes/solicitations_routes";
+import gamesRouter from "./routes/games_routes";
 
 const app = express();
 const port = 3000;
+app.use(express.json());
+
+app.use("/players", playersRouter);
+app.use("/games", gamesRouter);
+//app.use("/solicitations", solicitationsRouter);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Olá, mundo com TypeScript! 🚀");
